5fa0b83894c5040282ca5747f60e84fe3404a739
Kasie <jeoncas@gmail.com>
2016-02-06 01:01:02 +0000

    Ready

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -57,13 +57,4 @@ class GramsController < ApplicationController
   	def gram_params
     	params.require(:gram).permit(:message, :picture)
   	end
-
-
-  	def render_not_found
-    	render text: 'Not Found :(', status: :not_found
-  	end
-
-  	def render_not_found(status=:not_found)
-    	render text: "#{status.to_s.titleize} :(", status: status
-  	end
 end
